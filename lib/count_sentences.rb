1c2a873cc24961d8b2a1bfb4f310d30c3a56eba8
Paul Jung <pbnjcub@gmail.com>
2022-11-13 12:28:18 -0500

    lab done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,27 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    if self.length == 0
+      0
+    else
+      split_str = self.split
+      end_words = split_str.filter do |word|
+        word.end_with?("!") || word.end_with?(".") || word.end_with?("?")
+      end
+    end_words.length
+    end
   end
 end
