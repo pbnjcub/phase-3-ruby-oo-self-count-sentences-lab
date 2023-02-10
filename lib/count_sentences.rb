cb64dc7152671f0c5813ce59e3488440add93a5a
Paul Jung <pbnjcub@gmail.com>
2023-02-10 06:02:09 -0500

    commit tree fd9d61f3bbb5e3bf1bf95602c00437109cc0c9a5

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,4 +1,4 @@
-require 'pry'
+# require 'pry'
 
 class String
   def sentence?
@@ -25,3 +25,54 @@ class String
     end
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class String
+#   def sentence?
+#     self.end_with?(".")
+#   end
+
+#   def question?
+#     self.end_with?("?")
+#   end
+
+#   def exclamation?
+#     self.end_with?("!")
+#   end
+
+#   def count_sentences
+#     if self.length == 0
+#       0
+#     else
+#       split_str = self.split
+#       end_words = split_str.filter do |word|
+#         word.end_with?("!") || word.end_with?(".") || word.end_with?("?")
+#       end
+#     end_words.length
+#     end
+#   end
+# end
